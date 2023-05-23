112e2c7043be33f0632deee5d9bf13800208e05d
RH <f2hodge@gmail.com>
2023-05-23 12:15:17 -0400

    20230523

--- home.js
+++ home.js
@@ -1,3 +1,27 @@
+const header = document.getElementById('message');
+
+header.style.color = '#FFF';  // Makes color white
+header.style.fontWeight = '100'; // makes font thin
+
+const button = document.getElementById('see-review');
+
+button.addEventListener('click', function() {
+
+    console.log('click');
+    const review = document.getElementById('review');
+    const seeReviewButton = document.getElementById('see-review');
+
+    if(review.classList.contains('d-none')) {
+        showSeeReview("Hide Review");
+        review.classList.remove('d-none');
+
+    } else {
+        showSeeReview("See Review");
+        review.classList.add('d-none');
+    };
+    
+});
+
 let price = 49.99,
     name = "Hiking Boots",
     discounted = false;
--- utils.js
+++ utils.js
@@ -2,6 +2,10 @@ function showMessage(message) {
     document.getElementById('message').textContent = message;
 };
 
+function showSeeReview(seeReview) {
+    document.getElementById('see-review').textContent = seeReview;
+};
+
 function changePercentOff(percentage) {
     document.getElementById('percent-off').textContent = percentage + "% OFF";
 };
